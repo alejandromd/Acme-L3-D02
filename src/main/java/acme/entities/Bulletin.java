0ca5a3b4abcedffd226441e5944f463dde5fd10c
alejandromd <alepridur@alum.us.es>
2023-02-22 17:41:47 +0100

    arreglado problema en entidad bulletin Fixes #56

--- src/main/java/acme/entities/Bulletin.java
+++ src/main/java/acme/entities/Bulletin.java
@@ -3,6 +3,7 @@ package acme.entities;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -11,7 +12,12 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
 public class Bulletin extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
