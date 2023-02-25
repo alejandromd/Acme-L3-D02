1e4d85dc6625c3dc67769c6e673c374c67163a16
FranVargasCastro <50156906+FranVargasCastro@users.noreply.github.com>
2023-02-25 17:57:19 +0100

    fix:Arreglado atributos auditor
    Se ha modificado la longitud de las cadenas
    así como se ha añadido la restricción URL
    
    Refs#43

--- src/main/java/acme/roles/Auditor.java
+++ src/main/java/acme/roles/Auditor.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.validation.constraints.NotBlank;
 
 import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
 
 import acme.framework.data.AbstractRole;
 import lombok.Getter;
@@ -22,17 +23,18 @@ public class Auditor extends AbstractRole {
 	// Attributes -------------------------------------------------------------
 
 	@NotBlank
-	@Length(max = 76)
+	@Length(max = 75)
 	protected String			firm;
 
 	@NotBlank
-	@Length(max = 26)
+	@Length(max = 25)
 	protected String			professionalId;
 
 	@NotBlank
-	@Length(max = 101)
+	@Length(max = 100)
 	protected String			certifications;
 
+	@URL
 	protected String			link;
 
 	// Derived attributes -----------------------------------------------------
