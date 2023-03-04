c800033064b7ea19ddf0dce26f807b508aa08423
FranVargasCastro <50156906+FranVargasCastro@users.noreply.github.com>
2023-03-04 11:56:31 +0100

    feat:añadida relaciones en audit
    Se ha añadido la relación de audit con auditor

--- src/main/java/acme/entities/Audit.java
+++ src/main/java/acme/entities/Audit.java
@@ -3,12 +3,16 @@ package acme.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 
 import acme.framework.data.AbstractEntity;
+import acme.roles.Auditor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -40,4 +44,8 @@ public class Audit extends AbstractEntity {
 	// Derived attributes -----------------------------------------------------
 
 	// Relationships ----------------------------------------------------------
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Auditor			auditor;
 }
