aa9add90c8ee7ef2ec2e58c9946152dd28bdb66e
Thealean28 <alejandrogallardopelayo@gmail.com>
2023-03-06 18:23:00 +0100

    fix: Relacion enrolement y workbook

--- src/main/java/acme/entities/Workbook.java
+++ src/main/java/acme/entities/Workbook.java
@@ -1,15 +1,12 @@
 
 package acme.entities;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.OneToOne;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 
+import acme.datatypes.ActivityType;
 import acme.framework.data.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,33 +14,25 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class Enrolment extends AbstractEntity {
+public class Workbook extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
 	protected static final long	serialVersionUID	= 1L;
 
 	// Attributes -------------------------------------------------------------
 
-	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
-	@NotBlank
-	@Column(unique = true)
-	protected String			code;
-
 	@NotBlank
 	@Length(max = 75)
-	protected String			motivation;
+	protected String			title;
 
 	@NotBlank
 	@Length(max = 100)
-	protected String			goals;
+	protected String			abstract0;
 
-	protected Integer			workTime;
+	//@Enumerated
+	protected ActivityType		indication;
 
 	// Derived attributes -----------------------------------------------------
-
-	@OneToOne
-	@NotNull
-	protected Workbook			workbook;
 
 	// Relationships ----------------------------------------------------------
 
