ff2173d65162b0cba64613c03338aec1e1b9869c
Thealean28 <alejandrogallardopelayo@gmail.com>
2023-03-07 17:58:09 +0100

    fix:atributos Activity

--- src/main/java/acme/entities/Activity.java
+++ src/main/java/acme/entities/Activity.java
@@ -1,12 +1,21 @@
 
 package acme.entities;
 
+import java.sql.Date;
+
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
 
 import acme.datatypes.ActivityType;
+import acme.entities.Enrolment;
 import acme.framework.data.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,7 +23,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class Workbook extends AbstractEntity {
+public class Activity extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
 	protected static final long	serialVersionUID	= 1L;
@@ -29,11 +38,25 @@ public class Workbook extends AbstractEntity {
 	@Length(max = 100)
 	protected String			abstract0;
 
-	//@Enumerated
+	@NotNull
 	protected ActivityType		indication;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date				startPeriod;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date				endPeriod;
+
+	@URL
+	protected String			link;
 
 	// Derived attributes -----------------------------------------------------
 
 	// Relationships ----------------------------------------------------------
-
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Enrolment			workbook;
 }
