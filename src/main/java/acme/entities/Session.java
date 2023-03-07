e66ae99f0aae75b90fa2c612bcb46bf9eaf6c676
goncammej <goncammej@alum.us.es>
2023-03-07 21:31:57 +0100

    Feat: Add relationship with tutorial

--- src/main/java/acme/entities/Session.java
+++ src/main/java/acme/entities/Session.java
@@ -4,8 +4,10 @@ package acme.entities;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
@@ -47,4 +49,13 @@ public class Session extends AbstractEntity {
 
 	@URL
 	protected String			furtherInfo;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Tutorial			tutorial;
 }
