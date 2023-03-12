867eb178ed0217eac0921c7cd160685046975d73
FranVargasCastro <50156906+FranVargasCastro@users.noreply.github.com>
2023-03-12 20:01:18 +0100

    fix:eliminadas clausulas where en query auditorDashboard

--- src/main/java/acme/features/authenticated/auditor/AuditorDashboardRepository.java
+++ src/main/java/acme/features/authenticated/auditor/AuditorDashboardRepository.java
@@ -12,27 +12,27 @@ public interface AuditorDashboardRepository extends AbstractRepository {
 	@Query("select count(au) from Audit au where au.auditor.id = auditor.id")
 	Integer totalNumberOfAudits();
 
-	@Query("select stddev(select count(ar) from AuditingRecord ar where ar.au.id = au.id) from Audit au where au.auditor.id = auditor.id")
+	@Query("select stddev(select count(ar) from AuditingRecord ar where ar.au.id = au.id) from Audit au")
 	Double deviationNumberOfAuditingRecordsAudit();
 
-	@Query("select avg(select count(ar) from AuditingRecord ar where ar.au.id = au.id) from Audit au where au.auditor.id = auditor.id")
+	@Query("select avg(select count(ar) from AuditingRecord ar where ar.au.id = au.id) from Audit au")
 	Double averageNumberOfAuditingRecordsAudit();
 
-	@Query("select max(select count(ar) from AuditingRecord ar where ar.au.id = au.id) from Audit au where au.auditor.id = auditor.id")
+	@Query("select max(select count(ar) from AuditingRecord ar where ar.au.id = au.id) from Audit au")
 	Integer maximunNumberOfAuditingRecordsAudit();
 
-	@Query("select min(select count(ar) from AuditingRecord ar where ar.au.id = au.id) from Audit au where au.auditor.id = auditor.id")
+	@Query("select min(select count(ar) from AuditingRecord ar where ar.au.id = au.id) from Audit au")
 	Integer minimunNumberOfAuditingRecordsAudit();
 
-	@Query("select avg(select sum(ar.duration) from AuditingRecord ar where ar.au.id = au.id) from Audit au where au.auditor.id = auditor.id")
+	@Query("select avg(select sum(ar.duration) from AuditingRecord ar where ar.au.id = au.id) from Audit au")
 	Double averageTimeOfPeriodLengthAuditingRecords();
 
-	@Query("select stddev(select sum(ar.duration) from AuditingRecord ar where ar.au.id = au.id) from Audit au where au.auditor.id = auditor.id")
+	@Query("select stddev(select sum(ar.duration) from AuditingRecord ar where ar.au.id = au.id) from Audit au")
 	Double deviationTimeOfPeriodLengthAuditingRecords();
 
-	@Query("select min(select sum(ar.duration) from AuditingRecord ar where ar.au.id = au.id) from Audit au where au.auditor.id = auditor.id")
+	@Query("select min(select sum(ar.duration) from AuditingRecord ar where ar.au.id = au.id) from Audit au")
 	Double minimunTimeOfPeriodLengthAuditingRecords();
 
-	@Query("select max(select sum(ar.duration) from AuditingRecord ar where ar.au.id = au.id) from Audit au where au.auditor.id = auditor.id")
+	@Query("select max(select sum(ar.duration) from AuditingRecord ar where ar.au.id = au.id) from Audit au")
 	Double maximunTimeOfPeriodLengthAuditingRecords();
 }
