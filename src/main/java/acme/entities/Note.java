92f2555698899c945ab7681ded8297cb4145819d
FranVargasCastro <50156906+FranVargasCastro@users.noreply.github.com>
2023-03-14 11:29:29 +0100

    fix:añadida restricción NotNull en instantiationMoment

--- src/main/java/acme/entities/Note.java
+++ src/main/java/acme/entities/Note.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 
 import org.hibernate.validator.constraints.Length;
@@ -25,6 +26,7 @@ public class Note extends AbstractEntity {
 
 	// Attributes -------------------------------------------------------------
 	@Past
+	@NotNull
 	protected Date				instantiationMoment;
 
 	@NotBlank
