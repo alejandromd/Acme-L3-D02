81639b3bc2cc31b3190c721814472292306f297b
Thealean28 <alejandrogallardopelayo@gmail.com>
2023-03-15 20:05:40 +0100

    fix:atributo worktime

--- src/main/java/acme/entities/Enrolment.java
+++ src/main/java/acme/entities/Enrolment.java
@@ -39,6 +39,9 @@ public class Enrolment extends AbstractEntity {
 	@Length(max = 100)
 	protected String			goals;
 
+	@NotNull
+	protected Double			workTime;
+
 	// Derived attributes -----------------------------------------------------
 
 	// Relationships ----------------------------------------------------------
