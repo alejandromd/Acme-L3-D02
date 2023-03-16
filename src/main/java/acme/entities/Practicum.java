1c495c19f6e8778e6dd346113e11f6b114104b7d
alblopnov <alblopnov@alum.us.es>
2023-03-16 11:08:52 +0100

    subida de la entidad Fixes #40

---
+++ src/main/java/acme/entities/Practicum.java
@@ -0,0 +1,64 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.framework.data.AbstractEntity;
+import acme.roles.Company;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Practicum extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+	@NotBlank
+	@Pattern(regexp = "^[A-Z]{1,3}\\d{3}$")
+	@Column(unique = true)
+	protected String			code;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			abstractt;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			goals;
+
+	protected Date				initialTime;
+
+	protected Date				finalTime;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@Valid
+	@NotNull
+	@ManyToOne(optional = false)
+	protected Company			company;
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Course			course;
+}
