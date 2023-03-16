a0383f68c128146e1051f94608374c2b8131e087
goncammej <goncammej@alum.us.es>
2023-03-16 17:32:32 +0100

    Fix: Comment transient attribute

--- src/main/java/acme/entities/Tutorial.java
+++ src/main/java/acme/entities/Tutorial.java
@@ -5,7 +5,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -45,8 +44,9 @@ public class Tutorial extends AbstractEntity {
 	@Length(max = 100)
 	protected String			goals;
 
-	@Min(0)
-	protected int				estimatedTime;
+	/*
+	 * protected Double estimatedTime;
+	 */
 
 	// Derived attributes -----------------------------------------------------
 
