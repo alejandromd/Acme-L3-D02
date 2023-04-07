c67ecff8bbac9fd8e2c3d8272c605652753f2459
FranVargasCastro <50156906+FranVargasCastro@users.noreply.github.com>
2023-04-07 21:53:53 +0200

    feature:Implementadas operaciones sobre note
    Se han implementado las operaciones de crear, listar y ver
    detalles sobre note.
    
    Fixes#265

---
+++ src/main/java/acme/features/authenticated/note/AuthenticatedNoteCreateService.java
@@ -0,0 +1,89 @@
+
+package acme.features.authenticated.note;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.Note;
+import acme.framework.components.accounts.Authenticated;
+import acme.framework.components.accounts.Principal;
+import acme.framework.components.accounts.UserAccount;
+import acme.framework.components.models.Tuple;
+import acme.framework.helpers.MomentHelper;
+import acme.framework.services.AbstractService;
+
+@Service
+public class AuthenticatedNoteCreateService extends AbstractService<Authenticated, Note> {
+
+	@Autowired
+	protected AuthenticatedNoteRepository repository;
+
+
+	@Override
+	public void check() {
+		super.getResponse().setChecked(true);
+	}
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Note object;
+		Principal principal;
+		int userAccountId;
+		UserAccount userAccount;
+		Date moment;
+
+		principal = super.getRequest().getPrincipal();
+		userAccountId = principal.getAccountId();
+		userAccount = this.repository.findOneUserAccountById(userAccountId);
+		moment = MomentHelper.getCurrentMoment();
+
+		object = new Note();
+		object.setInstantiationMoment(moment);
+		object.setAuthor(userAccount.getUsername() + " - " + userAccount.getIdentity().getSurname() + ", " + userAccount.getIdentity().getName());
+
+		super.getBuffer().setData(object);
+	}
+
+	@Override
+	public void bind(final Note object) {
+		assert object != null;
+
+		super.bind(object, "instantiationMoment", "title", "author", "message", "email", "link");
+
+	}
+
+	@Override
+	public void validate(final Note object) {
+		assert object != null;
+
+		boolean confirmation;
+
+		confirmation = super.getRequest().getData("confirmation", boolean.class);
+		super.state(confirmation, "confirmation", "javax.validation.constraints.AssertTrue.message");
+	}
+
+	@Override
+	public void perform(final Note object) {
+		assert object != null;
+
+		this.repository.save(object);
+	}
+
+	@Override
+	public void unbind(final Note object) {
+		assert object != null;
+
+		Tuple tuple;
+
+		tuple = super.unbind(object, "instantiationMoment", "title", "author", "message", "email", "link");
+
+		super.getResponse().setData(tuple);
+	}
+}
