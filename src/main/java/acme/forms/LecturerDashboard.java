44192807b0b1b2b267a95fad29777087ed651d4c
alejandromd <alepridur@alum.us.es>
2023-04-11 16:05:36 +0200

    feat: first version of lecturer dashboard

--- src/main/java/acme/forms/LecturerDashboard.java
+++ src/main/java/acme/forms/LecturerDashboard.java
@@ -1,6 +1,8 @@
 
 package acme.forms;
 
+import java.util.Collection;
+
 import acme.framework.data.AbstractForm;
 import lombok.Getter;
 import lombok.Setter;
@@ -25,8 +27,38 @@ public class LecturerDashboard extends AbstractForm {
 	Double						minimumTimeOfCourses;
 	Double						maximumTimeOfCourses;
 
-	// Derived attributes -----------------------------------------------------
 
-	// Relationships ----------------------------------------------------------
+	public void calcAverage(final Collection<Double> values) {
+		Double res;
+		res = 0.0;
+		if (!values.isEmpty()) {
+			final Double total = values.stream().mapToDouble(Double::doubleValue).sum();
+			res = total / values.size();
+		}
+		this.averageTimeOfCourses = res;
+	}
+
+	public void calcMax(final Collection<Double> values) {
+		final Double max = values.stream().mapToDouble(Double::doubleValue).max().orElse(0.0);
+		this.maximumTimeOfCourses = max;
+	}
+
+	public void calcMin(final Collection<Double> values) {
+		final Double min = values.stream().mapToDouble(Double::doubleValue).min().orElse(0.0);
+		this.minimumTimeOfCourses = min;
+	}
+	public void calcLinDev(final Collection<Double> values) {
+		Double res;
+		Double aux;
+		res = 0.0;
+		if (!values.isEmpty()) {
+			aux = 0.0;
+			for (final Double value : values)
+				aux += Math.pow(value + this.averageTimeOfCourses, 2);
+			res = Math.sqrt(aux / values.size());
+		}
+		this.deviationTimeOfCourses = res;
+
+	}
 
 }
