833c009108a102281b3332e5d587ef4d48d0fb6b
FranVargasCastro <50156906+FranVargasCastro@users.noreply.github.com>
2023-05-25 16:42:51 +0200

    feat:Creados test delete

--- src/test/java/acme/testing/auditor/audit/AuditorAuditTestRepository.java
+++ src/test/java/acme/testing/auditor/audit/AuditorAuditTestRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 
 import acme.entities.Audit;
 import acme.entities.Course;
+import acme.entities.auditingRecord.AuditingRecord;
 import acme.framework.repositories.AbstractRepository;
 
 public interface AuditorAuditTestRepository extends AbstractRepository {
@@ -16,5 +17,11 @@ public interface AuditorAuditTestRepository extends AbstractRepository {
 
 	@Query("select c from Course c where c.code = :code")
 	Course findOneCourseByCourseCode(String code);
+
+	@Query("select ar from AuditingRecord ar where ar.audit.id = :id")
+	Collection<AuditingRecord> findManyAuditingRecordsByAuditId(int id);
+
+	@Query("select au from Audit au where au.code = :code")
+	Audit findOneAuditByCode(String code);
 
 }
